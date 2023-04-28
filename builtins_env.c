06bfb06d5a8fd4cbcfeba76ecc575e849d14aa08
Sikundi <sikundi499@gmail.com>
2023-04-28 13:10:17 +0300

    shell project

--- builtins_env.c
+++ builtins_env.c
@@ -11,13 +11,16 @@ int builtin_env(data_of_program *data)
 	char cpname[50] = {'\0'};
 	char *var_copy = NULL;
 
+	/* if not arguments */
 	if (data->tokens[1] == NULL)
 		print_environ(data);
 	else
 	{
 		for (i = 0; data->tokens[1][i]; i++)
+		{/* checks if exists a char = */
 			if (data->tokens[1][i] == '=')
 			{/* checks if exists a var with the same name and change its value*/
+			/* temporally */
 				var_copy = str_duplicate(env_get_key(cpname, data));
 				if (var_copy != NULL)
 					env_set_key(cpname, data->tokens[1] + i + 1, data);
@@ -52,6 +55,7 @@ int builtin_env(data_of_program *data)
  */
 int builtin_set_env(data_of_program *data)
 {
+	/* validate args */
 	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
 		return (0);
 	if (data->tokens[3] != NULL)
@@ -73,6 +77,7 @@ int builtin_set_env(data_of_program *data)
  */
 int builtin_unset_env(data_of_program *data)
 {
+	/* validate args */
 	if (data->tokens[1] == NULL)
 		return (0);
 	if (data->tokens[2] != NULL)
@@ -84,4 +89,4 @@ int builtin_unset_env(data_of_program *data)
 	env_remove_key(data->tokens[1], data);
 
 	return (0);
-}
+}	
